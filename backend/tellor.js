873cde3a44104b162564644d797731e3ebaecb0d
02Azure <agusdomas1st@gmail.com>
2021-02-25 22:58:31 +0700

    added modal

--- backend/tellor.js
+++ backend/tellor.js
@@ -1,5 +1,43 @@
-/*
+document.getElementById("postNewPostButton").addEventListener("click",addNewPost)
+document.getElementById("cancelButton").addEventListener("click",closePrompt)
+let addNewPostButton = document.getElementsByClassName("addNewPostButton")
+
+for(let n = 0;n<addNewPostButton.length; n++)
+  addNewPostButton[n].addEventListener("click",showPrompt)
 
 
-yak di si ni ya
-*/
+function showPrompt(){
+  //resetvalue
+  document.getElementById("postContent").value = ""
+  document.getElementById("postDeadline").value =""
+
+  document.getElementById("modal").classList.remove("hide");
+  document.getElementById("promptAddPost").classList.remove("hide")
+}
+
+//menambahkan post baru
+function addNewPost(event){
+  event.preventDefault()
+
+  let content = document.getElementById("postContent").value
+  let deadline = document.getElementById("postDeadline").value
+
+  closePrompt(event)
+ 
+  console.log(
+    {
+      content,
+      deadline
+    }
+  )
+
+  //add ke array baru
+  //re render
+}
+
+//menutup prompt
+function closePrompt(event){
+  event.preventDefault()
+  document.getElementById("modal").classList.add("hide");
+  document.getElementById("promptAddPost").classList.add("hide")
+}
